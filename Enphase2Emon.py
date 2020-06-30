1b07522a7bbf11b0237ff0453084b08b154ebc9c
StoicWeatherWatch <34807600+StoicWeatherWatch@users.noreply.github.com>
2020-06-29 18:08:44 -0700

    Testing v0.0.2

--- Enphase2Emon.py
+++ Enphase2Emon.py
@@ -10,14 +10,16 @@ ReceiveEnphaseStream.py
 WriteToEmoncmsHTTP.py
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "SWW"
 
-CONFIG_FILE = 'Enphase2emon.cfg'
+CONFIG_FILE = 'Enphase2Emon.cfg'
 
 # Local Files
 import ReceiveEnphaseStream
 import WriteToEmoncmsHTTP
+
+#from Screamer import Screamer
 
 import json
 import configparser
@@ -34,46 +36,55 @@ import time
 # yield
 # Receive yield and communicate to emon
 
+# Switch to Python Daemon
+# Catch Exceptions
+# Notice a lost stream
+# Switch prints to log 
+
 # Read config file
-# TODO user try except
-config = configparser.ConfigParser()
-config.read(CONFIG_FILE)
+# TODO use try except
+def MainLoop():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
 
-# print(config['Envoy']['ipadd'])
-# print(type(config['Envoy']['ipadd']))
-# print(config['Settings']['updateinterval'])
-# print(type(config['Settings'].getint('updateinterval')))
-# print(config['Settings']['smoothing'])
-# print(type(config['Settings'].getboolean('smoothing')))
+    # print(config['Envoy']['ipadd'])
+    # print(type(config['Envoy']['ipadd']))
+    # print(config['Settings']['updateinterval'])
+    # print(type(config['Settings'].getint('updateinterval')))
+    # print(config['Settings']['smoothing'])
+    # print(type(config['Settings'].getboolean('smoothing')))
 
-StripPrefix = config['Envoy'].getint('num_chars_strip')
+    StripPrefix = config['Envoy'].getint('num_chars_strip')
 
-PostInter = config['Settings'].getint('UpdateInterval')
+    PostInter = config['Settings'].getint('UpdateInterval')
 
-# Setup the object to post to emoncms
-poster = WriteToEmoncmsHTTP.EMONPostHTTP(CONFIG_FILE)
+    # Setup the object to post to emoncms
+    poster = WriteToEmoncmsHTTP.EMONPostHTTP(CONFIG_FILE)
 
-TimeLast = time.time()
+    TimeLast = time.time()
 
-# Loop over stream
-for LineIn in ReceiveEnphaseStream.DataStream(config['Envoy']['ipadd'],
-												config['Envoy']['path'],
-												config['Envoy']['userid'],
-												config['Envoy']['userpass']):
-    #print(LineIn.decode("utf-8")[6:])
-    #print('\n')
+    # Loop over stream
+    for LineIn in ReceiveEnphaseStream.DataStream(config['Envoy']['ipadd'],
+                                                    config['Envoy']['path'],
+                                                    config['Envoy']['userid'],
+                                                    config['Envoy']['userpass']):
+        #print(LineIn.decode("utf-8")[6:])
+        #print('\n')
     
-    # The output has initial characters that need to be striped. Thus StripPrefix
-    LineDict = json.loads(LineIn.decode("utf-8")[StripPrefix:])
-    #print(LineDict)
-    #print('\n')
-    #print(LineDict["production"]["ph-a"]["p"])
-    #print(type(LineDict["production"]["ph-a"]["p"]))
-    #print('\n')
-    if (time.time() - TimeLast) > PostInter:
-    	poster.PostToEMON(LineDict)
-    	TimeLast = time.time()
+        # The output has initial characters that need to be striped. Thus StripPrefix
+        LineDict = json.loads(LineIn.decode("utf-8")[StripPrefix:])
+        #print(LineDict)
+        #print('\n')
+        #print(LineDict["production"]["ph-a"]["p"])
+        #print(type(LineDict["production"]["ph-a"]["p"]))
+        #print('\n')
+        if (time.time() - TimeLast) > PostInter:
+            poster.PostToEMON(LineDict)
+            TimeLast = time.time()
     
-    
+
+if __name__ == "__main__":
+    print("Running Enphase2Emon")
+    MainLoop()
     
 
