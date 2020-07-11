c512ceba640b10a2df62cef9d2c1158db7b48aab
StoicWeatherWatch <34807600+StoicWeatherWatch@users.noreply.github.com>
2020-07-11 14:30:39 -0700

    v0.1.0

--- Enphase2Emon.py
+++ Enphase2Emon.py
@@ -1,20 +1,31 @@
 """
 Enphase2Emon will receive the /stream/meter chunked http stream from an 
-Enphase Envoy s and transfer the data to EmonCMS running locally.
+Enphase Envoy s and transfer the data to EmonCMS running locally. It is designed to run
+as a daemon on the same R Pi as EmonCMS but it will happily run on a different 
+linux box.
 
 
-Enphase2Emon.py is the core program.
+Enphase2Emon.py is the core.
 
 Also uses
 ReceiveEnphaseStream.py
 WriteToEmoncmsHTTP.py
+
+Config file expected. 
+
+Requires 
+requests
+systemd-python - Used for logging. 
+                    To remove this requirement, 
+                    remove the systemd journal logging handler.)
+
 """
 
-__version__ = "0.0.8"
+__version__ = "0.1.0"
 __author__ = "SWW"
 
 # In Daemon form, this must be given the full path. 
-CONFIG_FILE = '/home/pi/Enphase2emon/Enphase2Emon.cfg'
+CONFIG_FILE = '/home/pi/Enphase2Emon/Enphase2Emon.cfg'
 
 # Local Files
 import ReceiveEnphaseStream
@@ -27,43 +38,38 @@ import time
 import signal
 import sys
 import traceback
-
 import logging
+
+# Requires pip3 install systemd-python
 from systemd.journal import JournalHandler as systemdJournalHandler
 
 # Program Flow
-# Read config file
+# Read config file and setup logging
+# Initial setup
 # Open stream URL with authentication
-#  On fail try again every 60 sec
-#  On lost stream reattempt every 60
+#  On fail exit and daemon service will restart. Maybe.
+#  On lost stream exit and daemon service will restart. Maybe.
 # Receive data from stream 
-# Check formatting
+# Check formatting - Not yet
 # Check timing only update ever interval seconds
 # yield
 # Receive yield and communicate to emon
 
-# Switch to Python Daemon
-# Catch Exceptions
-# Notice a lost stream
+# TODO
+# Notice a lost stream (Time out in place. Maybe enough)
 
 
 
-
-
-# TODO use try except
 def MainLoop():
     #Setup Loging
     LG = logging.getLogger('Enphase2Emon')
     
-    #Scream.dbg('MainLoop','Have Screamer')
-    
+    # Get config file (It has logging settings)
     config = configparser.ConfigParser()
-    #Scream.dbg('MainLoop','Have configparser')
     
     # ConfigParser.read "Files that cannot be opened are silently ignored;       
-    # Return list of successfully read files.\n        "
+    # Returns list of successfully read files."
     ListOfRead = config.read(CONFIG_FILE)
-    #Scream.dbg('MainLoop','config.read found {}'.format(ListOfRead))
     
     # Continue with logging setup
     if config['Logging']['log_level'] == 'DEBUG':
@@ -77,23 +83,27 @@ def MainLoop():
     else:
         # default and if not correct config is ERROR
         LogLevel = logging.ERROR
-        
     
-        
+    # What the logs will look like
     LGformatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     
     # Are we logging to a file
-    if len(config['Logging']['Log_File']) > 1:
-        FLHandler = logging.FileHandler(config['Logging']['Log_File'])
-        FLHandler.setLevel(LogLevel)
-        FLHandler.setFormatter(LGformatter)
-        LG.addHandler(FLHandler)
+    if config['Logging']['Log_File']:
+        if len(config['Logging']['Log_File']) > 1:
+            FLHandler = logging.FileHandler(config['Logging']['Log_File'])
+            FLHandler.setLevel(LogLevel)
+            FLHandler.setFormatter(LGformatter)
+            LG.addHandler(FLHandler)
     
     # Logging to systemd journal
     if config['Logging'].getboolean('Log_To_systemd_journal', fallback=False):
         SJHandler = systemdJournalHandler(level=LogLevel)
         SJHandler.setFormatter(LGformatter)
         LG.addHandler(SJHandler)
+        
+    # Log nothing if no logger is set
+    if not LG.hasHandlers():
+        LG.addHandler(logging.NullHandler())
     
     # This line is necessary because we must also set the minimum for the logger overall
     LG.setLevel(LogLevel)
@@ -107,6 +117,7 @@ def MainLoop():
     #LG.info(' info test')
     #LG.debug(' debug test')
     #LG.exception(msg, *args, **kwargs)
+    
     
     # Signal Handeler - for exiting gracefully on SIGTERM
     SoftKillReceived = False
@@ -145,25 +156,22 @@ def MainLoop():
             LG.info('EXITING')
             sys.exit(0)
 
-    # print(config['Envoy']['ipadd'])
-    # print(type(config['Envoy']['ipadd']))
-    # print(config['Settings']['updateinterval'])
-    # print(type(config['Settings'].getint('updateinterval')))
-    # print(config['Settings']['smoothing'])
-    # print(type(config['Settings'].getboolean('smoothing')))
 
+    # Finish reading the config file
     StripPrefix = config['Envoy'].getint('num_chars_strip')
     LG.debug('MainLoop StripPrefix {}'.format(StripPrefix))
 
     PostInter = config['Settings'].getint('UpdateInterval')
     LG.debug('MainLoop PostInter {}'.format(PostInter))
+    
+    # Used to post only every PostInter seconds
+    TimeLast = time.time()
+    LG.debug('MainLoop TimeLast {}'.format(TimeLast))
+
 
     # Setup the object to post to emoncms
     poster = WriteToEmoncmsHTTP.EMONPostHTTP(CONFIG_FILE)
     LG.debug('MainLoop poster ready')
-
-    TimeLast = time.time()
-    LG.debug('MainLoop TimeLast {}'.format(TimeLast))
     
     GracefulExitCheck()
     
@@ -178,12 +186,13 @@ def MainLoop():
         
             LG.debug('MainLoop LineIn received')
         
+            # Turn the received stream chunk text into a python dictionary of data
             try:
                 # The output has initial characters that need to be striped. Thus StripPrefix
                 LineDict = json.loads(LineIn.decode("utf-8")[StripPrefix:])
                 LG.debug('MainLoop LineDict decoded')
             except json.JSONDecodeError as err:
-                LG.error('Error receiving decoding line in with JSON. Will quit and hopefully be restarted')
+                LG.error('Error decoding line in with JSON. Will quit and hopefully be restarted')
                 LG.debug(err.msg)
                 LG.debug(err.doc)
                 LG.debug(err.pos)
@@ -194,6 +203,7 @@ def MainLoop():
         
             GracefulExitCheck()
         
+            # Post the data to EmonCMS
             if (time.time() - TimeLast) > PostInter:
                 LG.debug('MainLoop time good - posting')
                 try:
@@ -214,7 +224,8 @@ def MainLoop():
                     LG.debug(repr(traceback.format_tb(exc_traceback)))
                     LG.error('EXITING with error 17')
                     sys.exit(17)
-                    
+                
+                # Posted successfully, reset the clock 
                 TimeLast = time.time()
         
             GracefulExitCheck()
@@ -236,26 +247,19 @@ def MainLoop():
         LG.error('EXITING with error 15')
         sys.exit(15)
         
+        
     LG.error('Error left infinite loop. Should never reach this point.')
     exc_type, exc_value, exc_traceback = sys.exc_info()
     LG.debug(traceback.format_exc())
     LG.debug(repr(traceback.format_tb(exc_traceback)))
     LG.error('EXITING with error 18')
-    # Standard exit codes
+    # Standard exit codes (These are not)
     # https://freedesktop.org/software/systemd/man/systemd.exec.html#id-1.20.8
     sys.exit(18) 
+
 
 if __name__ == "__main__":
     print("Running Enphase2Emon as __main__")
     MainLoop()
     
-#configparser.ConfigParser.read.__doc__
-#"Read and parse a filename or an iterable of filenames.\n\n
 
-# Files that cannot be opened are silently ignored; this is\n        
-# designed so that you can specify an iterable of potential\n        
-# configuration file locations (e.g. current directory, user's\n        
-# home directory, systemwide directory), and all existing\n        
-# configuration files in the iterable will be read.  A single\n        
-# filename may also be given.\n\n        
-# Return list of successfully read files.\n        "
